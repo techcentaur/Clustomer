1c0c199a00fb77ba809f36dc68f0795d4c154fef
VarunBhandia <varunbhandia@gmail.com>
2019-05-30 15:39:43 +0530

    Parser added

--- parser.py
+++ parser.py
@@ -1,2 +1,21 @@
 import kml2geojson
-kml2geojson.main.convert('3G_mumbai_grid_WK18.kml', 'json_files')
+import json
+import pprint
+
+kml2geojson.main.convert('tets.kml', 'json_files')
+pp = pprint.PrettyPrinter(indent=4)
+
+
+with open('json_files/tets.geojson', 'r+') as f:
+    data = json.load(f)
+    features = data["features"]
+    n = len(features)
+
+    for feature in features:
+        feature["properties"]["stroke"] =  "#000000"
+        feature["properties"]["fill-opacity"] =  "1.0"
+
+    f.seek(0)        
+    json.dump(data, f, indent=4)
+    f.truncate()
+# pp.pprint(features)
