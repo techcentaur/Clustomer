0eece0fb8fd2cd4c29539495069079ac5e63f57e
Ankit Solanki <ankit03june@gmail.com>
2019-07-02 13:42:32 +0530

    Setup loggers, folders, connections, and datafile pickled storage directories

--- fact.py
+++ fact.py
@@ -1,25 +1,39 @@
 """Application Factory"""
 
+import os
 import sqlite3
+import logging
+from datetime import datetime 
+
 from flask import Flask
 from _config import yml_data
 from flask_cors import CORS
 
-upload_folder = yml_data["path"]["upload_folder"]
+# setup logging handlers
+if not os.path.exists("./logs"):
+	os.makedirs("./logs")
+logfile_name = (str(datetime.now())).rsplit(":", 1)[0].replace(":", "-")
+logging.basicConfig(filename="./logs/" + "{}-logfile.log".format(logfile_name), level=logging.DEBUG)
+logger = logging.getLogger('LOG')
+
 
 # flask application initiation
+upload_folder = yml_data["path"]["upload_folder"]
 app = Flask(__name__)
 app.secret_key="secret key"
 app.config['UPLOAD_FOLDER'] = upload_folder
 app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
 
+
 # cross-platform headers
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# database connection
-conn = sqlite3.connect(yml_data["database"]["name"])
 
+# set database connection
+if not os.path.exists("./db"):
+	os.makedirs("./db")
+conn = sqlite3.connect("./db/"+yml_data["database"]["name"])
 ## create the table if it does not exists
 cur = conn.cursor()
 cur.execute("select name from sqlite_master where type='table' and name='"+yml_data["database"]["table_name"]+"'")
@@ -33,3 +47,8 @@ if len(result) == 0:
 			time DATETIME)''')
 	conn.commit()
 	# table created
+
+
+# for storing files in pickled form
+if not os.path.exists("./datafiles"):
+	os.makedirs("./datafiles")
