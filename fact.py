f5277187468344a4a8b40f39957cff981b6d89ce
Ankit Solanki <ankit03june@gmail.com>
2019-07-02 12:02:05 +0530

    Storing filenames in sqlite database and file in pickle format for better mainpulation of data

--- fact.py
+++ fact.py
@@ -1,18 +1,35 @@
 """Application Factory"""
 
+import sqlite3
 from flask import Flask
 from _config import yml_data
-
 from flask_cors import CORS
 
 upload_folder = yml_data["path"]["upload_folder"]
 
+# flask application initiation
 app = Flask(__name__)
-
 app.secret_key="secret key"
 app.config['UPLOAD_FOLDER'] = upload_folder
 app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
 
+# cross-platform headers
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# database connection
+conn = sqlite3.connect(yml_data["database"]["name"])
+
+## create the table if it does not exists
+cur = conn.cursor()
+cur.execute("select name from sqlite_master where type='table' and name='"+yml_data["database"]["table_name"]+"'")
+result = cur.fetchall()
+
+if len(result) == 0:
+	cur.execute(''' create table filenames 
+			(id INTEGER PRIMARY KEY,
+			kmlfile TEXT NOT NULL,
+			datafile TEXT NOT NULL,
+			time DATETIME)''')
+	conn.commit()
+	# table created
